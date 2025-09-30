926ec6e749b9abdede9ede03e6109a14e5128f83
sd-101 <sd.resumebuilding@gmail.com>
2025-09-30 16:35:28 +0530

    Add files via upload

---
+++ project/src/main.jsx
@@ -0,0 +1,19 @@
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import { BrowserRouter } from 'react-router-dom';
+import { ThemeProvider } from './contexts/ThemeContext.jsx';
+import App from './App.jsx';
+import './index.css';
+import { AuthProvider } from './contexts/AuthContext.jsx';
+
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    <ThemeProvider>
+      <BrowserRouter>
+        <AuthProvider>
+          <App />
+        </AuthProvider>
+      </BrowserRouter>
+    </ThemeProvider>
+  </StrictMode>
+);
